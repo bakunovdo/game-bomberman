f5e4ca70e0fd4eb37502b3eac286aad209dbe103
bakunovdo <bakunov.do@gmail.com>
2020-10-07 17:07:23 +0300

    Updates

---
+++ precache-manifest.627fddf374595eebe1b5eb7e1e61bf88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09aa8c3a419d3643b0192c3e31093c3f",
+    "url": "/game-bomberman/index.html"
+  },
+  {
+    "revision": "403d856022f920be928a",
+    "url": "/game-bomberman/static/css/main.dea84399.chunk.css"
+  },
+  {
+    "revision": "63b2d806d972f63f28cd",
+    "url": "/game-bomberman/static/js/2.d36fe9f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-bomberman/static/js/2.d36fe9f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "403d856022f920be928a",
+    "url": "/game-bomberman/static/js/main.c0970fe5.chunk.js"
+  },
+  {
+    "revision": "7d96cc02b4d96f08a0c5",
+    "url": "/game-bomberman/static/js/runtime-main.d1adc39f.js"
+  }
+]);
